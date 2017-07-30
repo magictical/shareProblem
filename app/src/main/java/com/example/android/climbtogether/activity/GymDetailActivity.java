b777235da5f9251387b6db5752e353ca3b6fbf90
Ian <magictical@gmail.com>
2017-07-30 13:19:03 +0900

    Add GymDetailActivity and icons in the drawable folder.

---
+++ app/src/main/java/com/example/android/climbtogether/activity/GymDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.climbtogether.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.climbtogether.R;
+
+public class GymDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gym_detail_view);
+    }
+}
