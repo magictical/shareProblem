31250ff316f38e8a1d9c8391e7250d59be27ae93
Ian <magictical@gmail.com>
2017-08-07 12:15:37 +0900

    Add GymDetailActivity

--- app/src/main/java/com/example/android/climbtogether/activity/GymDetailActivity.java
+++ app/src/main/java/com/example/android/climbtogether/activity/GymDetailActivity.java
@@ -7,9 +7,19 @@ import com.example.android.climbtogether.R;
 
 public class GymDetailActivity extends AppCompatActivity {
 
+    public static final String TAG = GymDetailActivity.class.getName();
+
+    public static final String EXTRA_GYM_DETAIL_KEY = "gym_detail_key";
+
+
+    private String mGymDetailKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gym_detail_view);
+
     }
+
+
+
 }
