3d72943ba510d660885b1b26ef8dca32f6f61ae0
Ian <magictical@gmail.com>
2017-08-07 12:13:00 +0900

    Change GymAdapter(customArrayListAdapter) to FirebaseRecyclerAdapter with ViewHolder

--- app/src/main/java/com/example/android/climbtogether/fragment/GymFragment.java
+++ app/src/main/java/com/example/android/climbtogether/fragment/GymFragment.java
@@ -1,21 +1,22 @@
 package com.example.android.climbtogether.fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListView;
 
 import com.example.android.climbtogether.Gym;
 import com.example.android.climbtogether.GymAdapter;
 import com.example.android.climbtogether.R;
+import com.example.android.climbtogether.activity.GymDetailActivity;
 import com.example.android.climbtogether.activity.GymResister;
+import com.example.android.climbtogether.viewHolder.ViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,9 +24,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Created by MD on 2017-03-09.
@@ -41,9 +39,16 @@ public class GymFragment extends Fragment {
     //Add Problem button
     Button mProblemResister;
 
+    //var Recycler
+    RecyclerView mRecyclerView;
+
     //add FireBaseDatabase
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mGymDatabaseReference;
+
+    private FirebaseRecyclerAdapter<Gym, ViewHolder>  mFirebaseRecyclerAdapter;
+
+    private  LinearLayoutManager mLinearLayoutManager;
 
     FirebaseStorage mFirebaseStorage;
     StorageReference mGymStorageReference;
@@ -60,13 +65,13 @@ public class GymFragment extends Fragment {
         //reference point of DB
         mGymDatabaseReference = mFirebaseDatabase.getReference().child("gym_data");
 
-        //add new Gym ArrayList
+        /*//add new Gym ArrayList
         ArrayList<Gym> gym = new ArrayList<Gym>();
         mGymAdapter = new GymAdapter(getActivity(), gym);
 
         //Bind ListView
         ListView list = (ListView) rootView.findViewById(R.id.gym_list);
-        list.setAdapter(mGymAdapter);
+        list.setAdapter(mGymAdapter);*/
 
 
         //Move to Gym resister activity when it clicked
@@ -79,7 +84,49 @@ public class GymFragment extends Fragment {
             }
         });
 
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.gym_list);
+        mRecyclerView.setHasFixedSize(true);
+
         return rootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mLinearLayoutManager = new LinearLayoutManager(getContext());
+        mLinearLayoutManager.setReverseLayout(true);
+        mLinearLayoutManager.setStackFromEnd(true);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        mFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Gym, ViewHolder>(
+                Gym.class,
+                R.layout.listitem_in_gym,
+                ViewHolder.class,
+                mGymDatabaseReference
+        ) {
+            @Override
+            protected void populateViewHolder(ViewHolder viewHolder, Gym model, int position) {
+                final DatabaseReference gymRef = getRef(position);
+                //Set click listener for the Gym detail view
+                final String gymDetailKey = gymRef.getKey();
+
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //launch gymDetailView
+                        Intent intent = new Intent(getActivity(), GymDetailActivity.class);
+                        intent.putExtra(GymDetailActivity.EXTRA_GYM_DETAIL_KEY, gymDetailKey);
+                        startActivity(intent);
+                    }
+                });
+
+                viewHolder.bindToGym(model);
+
+            }
+        };
+        mRecyclerView.setAdapter(mFirebaseRecyclerAdapter);
     }
 
     //리스트가 갱신될때 DB에서 새 data를 업데이트 하기위한 리스너
@@ -90,7 +137,7 @@ public class GymFragment extends Fragment {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                     Gym gym = dataSnapshot.getValue(Gym.class);
-                    mGymAdapter.add(gym);
+                    /*mFirebaseRecyclerAdapter.add(gym);*/
                 }
 
                 @Override
