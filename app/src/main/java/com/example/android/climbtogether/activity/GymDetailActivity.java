631747a914c4917eee1c9002ae2a43ff68b3565c
Ian <magictical@gmail.com>
2017-11-21 14:06:41 +0900

    Add button for to see the list of the problem which is registered in the specific gym. feature - when the button click, move to ProblemListFormGym activity, It request the putExtra and this will be used as a DB reference to access the problem lists in the DB

--- app/src/main/java/com/example/android/climbtogether/activity/GymDetailActivity.java
+++ app/src/main/java/com/example/android/climbtogether/activity/GymDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.climbtogether.activity;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -74,6 +75,16 @@ public class GymDetailActivity extends AppCompatActivity {
                 Intent intentToResisterPb = new Intent(GymDetailActivity.this, ProblemResister.class);
                 intentToResisterPb.putExtra(EXTRA_GYM_DETAIL_KEY, mGymDetailKey);
                 startActivity(intentToResisterPb);
+            }
+        });
+
+        //start fragment
+        mDetailSeeProblemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProblemListFromGym.class);
+                intent.putExtra(EXTRA_GYM_DETAIL_KEY, mGymDetailKey);
+                startActivity(intent);
             }
         });
 
